e66bad5a3475db192b3c1782d0f807d84b38e681
LorinLex <laizybot@gmail.com>
2021-04-15 15:07:38 +0300

    Add trans views & modify admin for tests

--- game/admin.py
+++ game/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
 from .models import SessionModel, PlayerModel, ProducerModel, BrokerModel, TransactionModel, BalanceDetail
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 
 @admin.register(PlayerModel)
 class PlayerAdmin(admin.ModelAdmin):
-    toket = admin
+    token = admin
     list_display = (
         "id",
         "nickname",
         "get_token",
-        "session",
-        "role",
+        "session_link",
+        "role_link",
         "city",
         "balance",
         "ended_turn",
         "is_bankrupt",
+    )
+
+    list_display_links = (
+        "nickname",
     )
 
     list_filter = (
@@ -26,6 +31,24 @@ class PlayerAdmin(admin.ModelAdmin):
         return mark_safe(f'<span>{obj.token.key}</span>');
 
     get_token.short_description = 'Токен'
+
+    def role_link(self, obj):
+        role_id = obj.producer.id if obj.role == 'producer' else obj.broker.id
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:game_{}model_change".format(str(obj.role)), args=(role_id,)),
+            obj.role
+        ))
+
+    role_link.short_description = 'Роль'
+
+    def session_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:game_sessionmodel_change", args=(obj.session_id,)),
+            obj.session.name
+        ))
+
+    session_link.short_description = 'Сессия'
+
 
 
 admin.site.register(SessionModel)
