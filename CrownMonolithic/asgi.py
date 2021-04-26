d448a59ef761f333e9c9b0edeb35af0a0414d267
LorinLex <laizybot@mail.com>
2021-04-26 14:55:40 +0300

    Removed custom Token auth ws middleware

--- CrownMonolithic/asgi.py
+++ CrownMonolithic/asgi.py
@@ -11,15 +11,16 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
-from game.middleware import TokenAuthMiddlewareStack
+# from game.middleware import TokenAuthMiddlewareStack
 from websockets import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrownMonolithic.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": TokenAuthMiddlewareStack(
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
         )
