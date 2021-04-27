0ca57c1e7e7b79498d3ed62fb1dc96efd16eeb83
LorinLex <laizybot@mail.com>
2021-04-27 12:13:39 +0300

    Modify consumer

--- websockets/routing.py
+++ websockets/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path
-from . import consumers
+from .consumers import LobbyConsumer, SessionConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/detail/(?P<session_id>\w+)/$', consumers.TimerConsumer.as_asgi()),
-    re_path(r'ws/lobby/', consumers.SessionConsumer.as_asgi())
+    re_path(r'ws/detail/(?P<session_id>\w+)/$', SessionConsumer.as_asgi()),
+    re_path(r'ws/lobby/', LobbyConsumer.as_asgi())
 ]
