86c0b450be985eb5ba3ddb53c6f4098c91b84a6a
LorinLex <laizybot@mail.com>
2021-05-11 11:25:48 +0300

    Change asgi

--- CrownMonolithic/asgi.py
+++ CrownMonolithic/asgi.py
@@ -1,25 +1,24 @@
 """
 ASGI config for CrownMonolithic project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-
-# from game.middleware import TokenAuthMiddlewareStack
-from websockets import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrownMonolithic.settings')
+asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from websockets import routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
