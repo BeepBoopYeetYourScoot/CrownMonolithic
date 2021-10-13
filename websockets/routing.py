2ffb8e57646e60a94684b6a12cda95939a0269b2
BeepBoopYeetYourScoot <dovakino98@gmail.com>
2021-10-13 11:53:08 +0500

    Пофиксил роутинг

--- websockets/routing.py
+++ websockets/routing.py
@@ -3,7 +3,7 @@ from .consumers import BrokerConsumer, ProducerConsumer, LobbyListConsumer, Lobb
 
 websocket_urlpatterns = [
     re_path(r'ws/detail/(?P<session_id>\w+)/$', LobbyDetailConsumer.as_asgi()),
-    re_path(r'ws/lobbies/', LobbyListConsumer.as_asgi()),
-    re_path(r'ws/broker_(?P<session_id>\w+)/$', BrokerConsumer.as_asgi()),
-    re_path(r'ws/producer_(?P<session_id>\w+)/$', ProducerConsumer.as_asgi()),
+    re_path(r'ws/lobby/', LobbyListConsumer.as_asgi()),
+    re_path(r'ws/broker_(?P<broker_id>\w+)/$', BrokerConsumer.as_asgi()),
+    re_path(r'ws/producer_(?P<producer_id>\w+)/$', ProducerConsumer.as_asgi()),
 ]
